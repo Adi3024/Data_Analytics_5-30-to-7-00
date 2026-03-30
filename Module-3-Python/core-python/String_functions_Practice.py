5835a212dfe33d00d4a34f9d8ebe53a25986b7ff
Adi3024 <shahdivyang1998@gmail.com>
2026-03-30 16:25:42 +0530

    String_Function_practice data uploaded

---
+++ Module-3-Python/core-python/String_functions_Practice.py
@@ -0,0 +1,33 @@
+str="My name is divyang"
+str.center(20, '*')
+print(str.center(20, '*'))
+
+
+str="I am 28 yeards old"
+str.find("28")
+print(str.find('28'))
+
+
+str="I am born in 1998."
+str.format()
+print(str.format())
+
+
+str= "I have studied Environmental Engineering" 
+str.isascii()
+print(str.isascii())
+
+
+str="I have 4.5 years of experience in the field of Environmental Engineering"
+str.split()
+print(str.split())
+
+
+str="Currently, I am working as an Environmental Engineer at a Khambhaliya Nagarpalika"
+str.title() 
+print(str.title())
+
+
+str="My name is Divyesh"
+str.replace("Divyesh", "Divyang")
+print(str.replace("Divyesh", "Divyang"))
